05122073aadcb1c2a0ee895cf17225c9c78e0a39
Arunkumar7092 <102907598+Arunkumar7092@users.noreply.github.com>
2023-07-06 12:21:09 +0530

    Added solution - LeetHub

--- Quick Left Rotation - GFG/quick-left-rotation.py
+++ Quick Left Rotation - GFG/quick-left-rotation.py
@@ -1,14 +1,14 @@
 class Solution:
+    
+
     def leftRotate(self, arr, k, n):
-        k = k % n
-        temp = [0]*k
+        k = k % n 
+        arr[0:k] =reversed(arr[0:k])
+        arr[k:n] = reversed(arr[k:n])
+        arr[0:n] = reversed(arr[0:n])
         
-        for i in range(0,k):
-            temp[i] = arr[i]
-        for i in range(k,n):
-            arr[i-k] = arr[i]
-        for i in range(n-k,n):
-            arr[i] = temp[i-(n-k)]        
+        
+       
 
 #{ 
  # Driver Code Starts
